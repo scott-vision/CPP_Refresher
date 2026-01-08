b99067282391782ecf417d23c9afa08af50b15d5
Scott Brooks <S.Brooks.2@warwick.ac.uk>
2026-01-08 10:33:42 +0000

    Actual conflict B

--- CPP_Refresher/CPP_Refresher.cpp
+++ CPP_Refresher/CPP_Refresher.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello New Potential Conflict!\n";
+    std::cout << "Hello New Potential Conflict B!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
